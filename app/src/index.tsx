d09c394a91805f70fff4bb9917af5404ee78a012
Peter Matthews <peter@pjmatthews.dev>
2021-07-02 16:26:50 -0500

    uninstall redux

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 
-import store from './redux';
 import './index.scss';
 import reportWebVitals from './utils';
 import { App } from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
